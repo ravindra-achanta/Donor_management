2a7870a0514e7ef5cf0c39c7ca6738329a8e488d
ravindra-achanta <ravindra.a@bytesedge.com>
2026-04-13 10:37:56 +0530

    new build on 13-04-26

--- lib/api_services/api_constants.dart
+++ lib/api_services/api_constants.dart
@@ -1,8 +1,7 @@
 class ApiConstants {
   // static const String baseUrl = "http://r:8050";
-  //static const String baseUrl = "https://vikas.vidyaranyam.in";
-      static const String baseUrl = "http://172.235.18.48:8050";
-
+  static const String baseUrl = "https://vikas.vidyaranyam.in";
+  // static const String baseUrl = "http://172.235.18.48:8050";
 
   static const String VIKAS = "/vikas/api/v1";
   static const String IDM_URI = "/vikas/api/v1/idm";
@@ -14,7 +13,7 @@ class ApiConstants {
   // Endpoints
   static const String GET_KARYAKARTHAS = "${IDM_URI}/users/all";
   static const String GET_KARYAKARTHAS_BY_ID = "${IDM_URI}/user";
-   static const String GET_OFFICE_STAFF ="${IDM_URI}/users/all";
+  static const String GET_OFFICE_STAFF = "${IDM_URI}/users/all";
   static const String GET_OFFICE_STAFF_BY_ID = "${IDM_URI}/user";
 
   //login
@@ -83,7 +82,8 @@ class ApiConstants {
   //activity/callLogs
   static const String ACTIVITY_LIST = "$VIKAS/callLogs/list";
   static const String ACTIVITY_CREATE = "$VIKAS/callLogs/create";
-static const String ACTIVITY_USER_DASHBOARD = "$VIKAS/activity/user/{id}/activity";
+  static const String ACTIVITY_USER_DASHBOARD =
+      "$VIKAS/activity/user/{id}/activity";
 
   static const String DELETE_USER = "$IDM_URI/delete/user";
 
